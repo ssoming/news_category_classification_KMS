c0157a208b0cc98ba7567dead0780897b6096f19
ming <chocolatte8003@gmail.com>
2026-06-04 16:33:32 +0900

    job02

---
+++ job02_crawling_data.py
@@ -0,0 +1,45 @@
+# file name: naver_news_section.csv
+# 컬럼 명은 titles, category
+# 00: 정치, 경제 / 01: 사회, 문화 / 02: 세계, IT
+# 다 되면 PR
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import re           # 내장 라이브러리
+import datetime     # 내장 라이브러리
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = ChromeOptions()
+options.add_argument('lang=ko_KR')
+options.add_argument('headless')    # browser window off
+
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+url = 'https://news.naver.com/section/100'
+driver.get(url)
+button_xpath = '//*[@id="newsct"]/div[4]/div/div[2]/a'
+# '//*[@id="newsct"]/div[4]/div/div[2]/a' # 뉴스 더보기에 대한 버튼에 대한 Xpath
+
+titles = []
+
+for i in range(5):
+    driver.find_element(By.XPATH, button_xpath).click()
+    time.sleep(0.5)
+
+for i in range(1, (5)):
+    for j in range(1, 7):
+        try:
+            title_xpath = '//*[@id="newsct"]/div[4]/div/div[1]/div[{}]/ul/li[{}]/div/div/div[2]/a/strong'.format(i,j)
+            title = driver.find_element(By.XPATH, title_xpath).text
+            print(title)
+            titles.append(title)
+        except:
+            print('error',i, j)
